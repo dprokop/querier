55de747dbadecc296cfe99a3adc379ffa4cd0495
Dominik Prokop <prokopd@gmail.com>
2018-02-25 00:18:48 +0100

    chore: Update email address in license and gh-pages publish

--- tools/gh-pages-publish.ts
+++ tools/gh-pages-publish.ts
@@ -23,7 +23,7 @@ touch(".nojekyll")
 exec("git init")
 exec("git add .")
 exec('git config user.name "Dominik Prokop"')
-exec('git config user.email "dominik@fortedigital.no"')
+exec('git config user.email "prokopd@gmail.com"')
 exec('git commit -m "docs(docs): update gh-pages"')
 exec(
   `git push --force --quiet "https://${ghToken}@${repository}" master:gh-pages`
