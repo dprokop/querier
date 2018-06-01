3319265e1c3ffa0722547820660ff2ad91be59ea
Dominik Prokop <prokopd@gmail.com>
2018-06-01 16:00:51 +0200

    Do not execute lazy queries on mount

--- src/withDataFactory.tsx
+++ src/withDataFactory.tsx
@@ -28,192 +28,189 @@ export const withDataFactory = <TProps, TInputQueries, TActionQueries>(queries: 
 }): InferableComponentEnhancer<TProps, TInputQueries, TActionQueries> => (
   Component: React.ComponentType<WithDataProps<TProps, TInputQueries, TActionQueries>>
 ) => {
-  class WithData extends React.Component<TProps> {
-    static displayName = `WithData(${getComponentDisplayName(Component)})`;
-    static contextTypes = {
-      querier: PropTypes.object
-    };
+    class WithData extends React.Component<TProps> {
+      static displayName = `WithData(${getComponentDisplayName(Component)})`;
+      static contextTypes = {
+        querier: PropTypes.object
+      };
 
-    private hasMounted: boolean = false;
-    private querierSubscriptions: Array<() => void> = [];
-    private propsToQueryKeysMap: Map<string, string> = new Map();
-    private executors: InputQueriesProps<TInputQueries>;
+      private hasMounted: boolean = false;
+      private querierSubscriptions: Array<() => void> = [];
+      private propsToQueryKeysMap: Map<string, string> = new Map();
+      private executors: InputQueriesProps<TInputQueries>;
 
-    context: QuerierProviderContext;
+      context: QuerierProviderContext;
 
-    constructor(props: TProps, context: QuerierProviderContext) {
-      super(props, context);
-      this.handleQuerierUpdate = this.handleQuerierUpdate.bind(this);
-      this.initializeInputQueriesExecutors = this.initializeInputQueriesExecutors.bind(this);
-      this.initializePropsToQueryKeysMap();
-      this.initializeInputQueriesExecutors();
-
-      invariant(
-        context.querier,
-        'Querier is not available in the context. Make sure you have wrapped your root component ' +
-          'with QuerierProvider'
-      );
-    }
-
-    componentDidMount() {
-      this.fireInputQueries();
-      this.hasMounted = true;
-    }
-
-    componentDidUpdate(prevProps: TProps) {
-      if (!shallowequal(this.props, prevProps)) {
-        this.unsubscribeQuerier();
+      constructor(props: TProps, context: QuerierProviderContext) {
+        super(props, context);
+        this.handleQuerierUpdate = this.handleQuerierUpdate.bind(this);
+        this.initializeInputQueriesExecutors = this.initializeInputQueriesExecutors.bind(this);
+        this.initializePropsToQueryKeysMap();
         this.initializeInputQueriesExecutors();
-        this.fireInputQueries(true);
+
+        invariant(
+          context.querier,
+          'Querier is not available in the context. Make sure you have wrapped your root component ' +
+          'with QuerierProvider'
+        );
       }
-    }
 
-    componentWillUnmount() {
-      this.unsubscribeQuerier();
-      this.hasMounted = false;
-    }
+      componentDidMount() {
+        this.fireInputQueries();
+        this.hasMounted = true;
+      }
 
-    initializePropsToQueryKeysMap() {
-      const { inputQueries } = queries;
+      componentDidUpdate(prevProps: TProps) {
+        if (!shallowequal(this.props, prevProps)) {
+          this.unsubscribeQuerier();
+          this.initializeInputQueriesExecutors();
+          this.fireInputQueries(true);
+        }
+      }
 
-      if (inputQueries) {
-        for (let prop in inputQueries) {
-          if (prop) {
-            const queryKey = `${inputQueries[prop].key}:${JSON.stringify(this.props)}`;
-            this.propsToQueryKeysMap.set(prop, queryKey);
+      componentWillUnmount() {
+        this.unsubscribeQuerier();
+        this.hasMounted = false;
+      }
+
+      initializePropsToQueryKeysMap() {
+        const { inputQueries } = queries;
+
+        if (inputQueries) {
+          for (let prop in inputQueries) {
+            if (prop) {
+              const queryKey = `${inputQueries[prop].key}:${JSON.stringify(this.props)}`;
+              this.propsToQueryKeysMap.set(prop, queryKey);
+            }
           }
         }
       }
-    }
 
-    fireInputQueries(includeLazy?: boolean) {
-      for (const executor in this.executors) {
-        if (executor) {
-          this.executors[executor].fire();
+      fireInputQueries(includeLazy?: boolean) {
+        for (const executor in this.executors) {
+          if (executor) {
+            if (!this.executors[executor].isLazy || (this.executors[executor].isLazy && includeLazy)) {
+              this.executors[executor].fire();
+            }
+          }
         }
       }
-    }
 
-    unsubscribeQuerier() {
-      this.querierSubscriptions = this.querierSubscriptions.filter(unsubscribe => {
-        return unsubscribe();
-      });
-    }
-
-    handleQuerierUpdate(queryData: QuerierStateEntry<{}>) {
-      if (this.hasMounted) {
-        this.setState({});
+      unsubscribeQuerier() {
+        this.querierSubscriptions = this.querierSubscriptions.filter(unsubscribe => {
+          return unsubscribe();
+        });
       }
-    }
 
-    buildComponentPropsFromResults() {
-      let props = {
-        results: {},
-        states: {}
-      };
-      this.propsToQueryKeysMap.forEach((queryKey, prop) => {
-        const queryStoreEntry = this.context.querier.getEntry(queryKey);
-        const result: {
-          [key: string]: {} | null;
-        } = {};
-        const states: {
-          [key: string]: {} | null;
-        } = {};
-        result[prop] = queryStoreEntry && queryStoreEntry.result;
-        states[prop] = queryStoreEntry && queryStoreEntry.state;
+      handleQuerierUpdate(queryData: QuerierStateEntry<{}>) {
+        if (this.hasMounted) {
+          this.setState({});
+        }
+      }
 
-        props = {
-          results: {
-            ...props.results,
-            ...result
-          },
-          states: {
-            ...props.states,
-            ...states
-          }
+      buildComponentPropsFromResults() {
+        let props = {
+          results: {},
+          states: {}
         };
-      });
-      return props as {
-        results: InjectedResults<TInputQueries, TActionQueries>;
-        states: InjectedStates<TInputQueries, TActionQueries>;
-      };
-    }
+        this.propsToQueryKeysMap.forEach((queryKey, prop) => {
+          const queryStoreEntry = this.context.querier.getEntry(queryKey);
+          const result: {
+            [key: string]: {} | null;
+          } = {};
+          const states: {
+            [key: string]: {} | null;
+          } = {};
+          result[prop] = queryStoreEntry && queryStoreEntry.result;
+          states[prop] = queryStoreEntry && queryStoreEntry.state;
 
-    getWrappedActionQueries() {
-      const { actionQueries } = queries;
-      const { querier } = this.context;
-      let wrappedActionQueries = {};
-
-      if (actionQueries) {
-        for (let actionQueryProp in actionQueries) {
-          if (actionQueryProp) {
-            const wrappedActionQuery: {
-              [key: string]: <TArgs>(args: TArgs) => void;
-            } = {};
-
-            wrappedActionQuery[actionQueryProp] = actionQueryParams => {
-              const query = () => actionQueries[actionQueryProp].query(actionQueryParams);
-              const queryKey = `${actionQueries[actionQueryProp].key}:${JSON.stringify(
-                actionQueryParams
-              )}`;
-              this.propsToQueryKeysMap.set(actionQueryProp, queryKey);
-
-              querier.subscribe(queryKey, this.handleQuerierUpdate);
-              querier.sendQuery({
-                query,
-                queryKey,
-                hot: actionQueries[actionQueryProp].hot,
-                props: actionQueryParams,
-                reason: getComponentDisplayName(Component)
-              });
-            };
-
-            wrappedActionQueries = {
-              ...wrappedActionQueries,
-              ...wrappedActionQuery
-            };
-          }
-        }
+          props = {
+            results: {
+              ...props.results,
+              ...result
+            },
+            states: {
+              ...props.states,
+              ...states
+            }
+          };
+        });
+        return props as {
+          results: InjectedResults<TInputQueries, TActionQueries>;
+          states: InjectedStates<TInputQueries, TActionQueries>;
+        };
       }
-      return wrappedActionQueries as ActionQueriesProps<TActionQueries>;
-    }
 
-    initializeInputQueriesExecutors(includeLazy?: boolean) {
-      const { querier } = this.context;
-      const { inputQueries } = queries;
+      getWrappedActionQueries() {
+        const { actionQueries } = queries;
+        const { querier } = this.context;
+        let wrappedActionQueries = {};
 
-      let executors = {};
-      if (inputQueries) {
-        for (let prop in inputQueries) {
-          if (prop) {
-            const query = () => inputQueries[prop].query(this.props);
-            const queryKey = `${inputQueries[prop].key}:${JSON.stringify(this.props)}`;
-            this.propsToQueryKeysMap.set(prop, queryKey);
+        if (actionQueries) {
+          for (let actionQueryProp in actionQueries) {
+            if (actionQueryProp) {
+              const wrappedActionQuery: {
+                [key: string]: <TArgs>(args: TArgs) => void;
+              } = {};
 
-            this.querierSubscriptions.push(querier.subscribe(queryKey, this.handleQuerierUpdate));
-            const executor: {
-              [key: string]: InputQueryExecutor;
-            } = {};
+              wrappedActionQuery[actionQueryProp] = actionQueryParams => {
+                const query = () => actionQueries[actionQueryProp].query(actionQueryParams);
+                const queryKey = `${actionQueries[actionQueryProp].key}:${JSON.stringify(
+                  actionQueryParams
+                )}`;
+                this.propsToQueryKeysMap.set(actionQueryProp, queryKey);
 
-            executor[prop] = {
-              isLazy: inputQueries[prop].lazy,
-              fire: (() => {
+                querier.subscribe(queryKey, this.handleQuerierUpdate);
                 querier.sendQuery({
                   query,
                   queryKey,
-                  props: this.props,
-                  reason: getComponentDisplayName(Component),
-                  effects: inputQueries[prop].resultActions,
-                  hot: !!inputQueries[prop].hot
+                  hot: actionQueries[actionQueryProp].hot,
+                  props: actionQueryParams,
+                  reason: getComponentDisplayName(Component)
                 });
-              }).bind(this)
-            };
-
-            if (inputQueries[prop].lazy && !includeLazy) {
-              executors = {
-                ...executors
               };
-            } else {
+
+              wrappedActionQueries = {
+                ...wrappedActionQueries,
+                ...wrappedActionQuery
+              };
+            }
+          }
+        }
+        return wrappedActionQueries as ActionQueriesProps<TActionQueries>;
+      }
+
+      initializeInputQueriesExecutors(includeLazy?: boolean) {
+        const { querier } = this.context;
+        const { inputQueries } = queries;
+
+        let executors = {};
+        if (inputQueries) {
+          for (let prop in inputQueries) {
+            if (prop) {
+              const query = () => inputQueries[prop].query(this.props);
+              const queryKey = `${inputQueries[prop].key}:${JSON.stringify(this.props)}`;
+              this.propsToQueryKeysMap.set(prop, queryKey);
+
+              this.querierSubscriptions.push(querier.subscribe(queryKey, this.handleQuerierUpdate));
+              const executor: {
+                [key: string]: InputQueryExecutor;
+              } = {};
+
+              executor[prop] = {
+                isLazy: inputQueries[prop].lazy,
+                fire: (() => {
+                  querier.sendQuery({
+                    query,
+                    queryKey,
+                    props: this.props,
+                    reason: getComponentDisplayName(Component),
+                    effects: inputQueries[prop].resultActions,
+                    hot: !!inputQueries[prop].hot
+                  });
+                }).bind(this)
+              };
+
               executors = {
                 ...executors,
                 ...executor
@@ -221,24 +218,23 @@ export const withDataFactory = <TProps, TInputQueries, TActionQueries>(queries: 
             }
           }
         }
+
+        this.executors = executors as InputQueriesProps<TInputQueries>;
       }
 
-      this.executors = executors as InputQueriesProps<TInputQueries>;
+      render() {
+        const { results, states } = this.buildComponentPropsFromResults();
+        return (
+          <Component
+            results={results}
+            actionQueries={this.getWrappedActionQueries()}
+            inputQueries={this.executors}
+            states={states}
+            {...this.props}
+          />
+        );
+      }
     }
 
-    render() {
-      const { results, states } = this.buildComponentPropsFromResults();
-      return (
-        <Component
-          results={results}
-          actionQueries={this.getWrappedActionQueries()}
-          inputQueries={this.executors}
-          states={states}
-          {...this.props}
-        />
-      );
-    }
-  }
-
-  return WithData;
-};
+    return WithData;
+  };
